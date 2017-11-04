72e11e2973fb65df5456189b95ac098eac1d6152
RickS1 <tenoriole1993@gmail.com>
2017-11-04 17:35:50 -0600

    Cuerpo

--- app/assets/javascripts/inicio.js
+++ app/assets/javascripts/inicio.js
@@ -1,5 +1,5 @@
 $(document).on("ready page:load",function(){
-	$("#cont-h").niceScroll({
+	$("body").niceScroll({
 		cursorwidth: "3px",
 		background: "rgba(150,150,150,0.6)",
 		cursorcolor: "red"
